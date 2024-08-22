46c5670aba118f9aa3cb1d920d13c7d810cef14b
wenhuanhou <wenhuanh@metropolia.fi>
2024-08-22 11:23:47 +0300

    ok

--- hello.py
+++ hello.py
@@ -1,4 +1,24 @@
+
+"""1
 print("Hello world")
 print(123)
 print("I love Finland")
 print("Hello,Hou Wenhuan")
+"""
+
+"""2
+user=input("Enter your name:")
+print("Nice to meet you, "  +user+"!")
+"""
+
+first = -9
+second = 12_456_123_180
+third = 4.973
+fourth = -4 + 2j
+
+print(first)
+print(second)
+print(third)
+print(fourth)
+print(fourth.real)
+print(fourth.imag)
